78fed34aaff2c974f29f1b26de6e3c964216f47d
Bradly Alicea <freejumper@yahoo.com>
2012-11-09 15:34:46 -0500

    Update

--- createtime.m
+++ createtime.m
@@ -1,12 +1,19 @@
 function data = createtime(samprate, reclength, measure)
+%%
 % creates a time stamp for the imported data. samprate = Hz, reclength =
 % length of recording in seconds, measure = name of recording type (i.e.
 % EEG, EMG, ECG)
+%%
 y = [0:1/(samprate):(reclength)];
 y = y';
 data = y
+%%
 % creates a 'timer' based on length of recording and sampling rate.
 % data = horzcat(y,x(1).data);
 % time = (:,1)
 % measure = (:,2)
 % create single matrix of data with timestamp.
+%%
+% Please use responsibly. Courtesy (2006) of Orthogonal Research,
+% bradly.alicea@outlook.com
+%%
--- event-related-extraction.m
+++ event-related-extraction.m
@@ -1,4 +1,5 @@
 function extract('signal matrix',x,y)
+%%
 % event-related extraction for Biopac.
 % inputs are: signal matrix = input time-series; organized with 'time' in the
 % first column, x = lower bound on time search, y = upper-bound on
@@ -10,39 +11,59 @@ function extract('signal matrix',x,y)
 %      .4  .21  .05
 %      .5  .13  .11
 %      .6  .10  .13]
+%%
 time = 'signal matrix'(:,1);
+%%
 % you must name the time column of the new matrix (or the column you want 
 % to search on). Change 'A' in each line of code to your matrix name. 
+%%
 m = time >=(x) & time <=(y);
+%%
 % select all cases between .3 and .5 in the 'time' column and code them 
 % "1". Creates a new binary matrix of size 1 x n called "x". Now, the 
 % dataset has a 'sorting' value based on selected time intervals.
- N = [m,A];
+%%
+N = [m,A];
+%%
 % combine the sorting variable into the dataset. New matrix "N" created. 
 % Again, note that 'A' can be replaced by the name of your matrix.
- N = sortrows(N);
+%%
+N = sortrows(N);
+%%
 % sort all of the columns in the new matrix by the first column (the
 % sorting variable).
- B = N(find(N(:,1)==1),:);
+%%
+B = N(find(N(:,1)==1),:);
+%%
 % creates a separate matrix 'B' based on the sorting variable. All 'ones', 
 % and only ones, will be included.
 % if you need to create a matrix of only 'zeros', the last line of code
 % would read:
- B = N(find(N(:,1)==0),:);
- % say you have run multiple "time" searches and extracted selected data
- % from the main matrix. To concantonate all of these time slices into a
- % single dataset, use this line:
- M = [B;C;D]
- % to do multiple searches
- % ya = time >=4 & time <=7;
- % yb = time >=14 & time <=35;
- % yc = time >=41 & time <=46;
- % put them all into a single matrix, including null moments
- Z = Y(find(Y(:,1)==1),:);
- ZB = Y(find(Y(:,2)==1),:);
- ZC = Y(find(Y(:,3)==1),:);
- % cut out all null moments; matrix of sorted time segments with
- % group that they belong to.
- Y = [ya;yb;yc]
- % concantonate matrix
- 
+%%
+B = N(find(N(:,1)==0),:);
+%%
+% say you have run multiple "time" searches and extracted selected data
+% from the main matrix. To concantonate all of these time slices into a
+% single dataset, use this line:
+%%
+M = [B;C;D]
+%%
+% to do multiple searches
+% ya = time >=4 & time <=7;
+% yb = time >=14 & time <=35;
+% yc = time >=41 & time <=46;
+% put them all into a single matrix, including null moments
+%%
+Z = Y(find(Y(:,1)==1),:);
+ZB = Y(find(Y(:,2)==1),:);
+ZC = Y(find(Y(:,3)==1),:);
+%%
+% cut out all null moments; matrix of sorted time segments with
+% group that they belong to.
+%%
+Y = [ya;yb;yc]
+% concantonate matrix
+%%
+% Please use responsibly. Courtesy (2006) of Orthogonal Research,
+% bradly.alicea@outlook.com
+%%
